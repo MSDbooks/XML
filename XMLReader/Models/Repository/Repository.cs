a47b724d71d836744903e416a5af86a5c63fb2a2
Marcelo Santos <marcelo.saantoos@hotmail.com>
2018-09-24 20:58:15 -0300

    Repository

---
+++ XMLReader/Models/Repository/Repository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace XMLReader.Models.Repository
+{
+    public class Repository : IRepository, IDisposable
+    {
+
+        private String strConnection;
+        private SqlConnection connection;
+
+        public Repository()
+        {
+            strConnection = System.Configuration.ConfigurationManager.ConnectionStrings["LiveRetail"].ConnectionString;
+        }
+
+        public SqlConnection GetConnection()
+        {
+            if (connection == null)
+                connection = new SqlConnection(strConnection);
+
+            return connection;
+                 
+        }
+
+        public void Dispose()
+        {
+
+            if (connection != null)
+                connection.Dispose();
+
+            connection = null;
+
+        }
+        
+    }
+}
